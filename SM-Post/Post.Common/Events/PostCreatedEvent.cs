472f8a7b6a0d946c1e2cedddff28b6a5156a0955
Sid <oxyprogrammer@gmail.com>
2024-04-19 10:55:36 +0530

    Added Commands and events.

---
+++ SM-Post/Post.Common/Events/PostCreatedEvent.cs
@@ -0,0 +1,19 @@
+﻿
+
+using CQRS.Core.Events;
+
+namespace Post.Common.Events
+{
+    public class PostCreatedEvent : BaseEvent
+    {
+        public PostCreatedEvent() : base(nameof(PostCreatedEvent))
+        {
+        }
+
+        public string Author { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime DatePosted { get; set; }
+    }
+}
---
+++ SM-Post/Post.Common/Events/PostRemovedEvent.cs
@@ -0,0 +1,13 @@
+﻿
+
+using CQRS.Core.Events;
+
+namespace Post.Common.Events
+{
+    public class PostRemovedEvent : BaseEvent
+    {
+        public PostRemovedEvent() : base(nameof(PostRemovedEvent))
+        {
+        }
+    }
+}
