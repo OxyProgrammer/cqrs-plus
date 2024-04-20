1ec3db93991ad94edaff7fe2edfd4d0396d4a1a6
Sid <oxyprogrammer@gmail.com>
2024-04-20 23:32:08 +0530

    Started with Db context

---
+++ SM-Post/Post.Query/Post.Query.Infrastructure/DataAccess/DatabaseContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Post.Query.Domain.Entities;
+
+namespace Post.Query.Infrastructure.DataAccess
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<PostEntity> Posts { get; set; }
+
+        public DbSet<CommentEntity> Comments { get; set; }
+    }
+
+    public class DatabaseContextFactory
+    {
+        private readonly Action<DbContextOptionsBuilder> _configureDbContext;
+
+        public DatabaseContextFactory(Action<DbContextOptionsBuilder> configureDbContext)
+        {
+            _configureDbContext = configureDbContext;
+        }
+
+        public DatabaseContext CreateDbContext()
+        {
+            DbContextOptionsBuilder<DatabaseContext> options = new();
+            _configureDbContext(options);
+
+            return new DatabaseContext(options.Options);
+        }
+    }
+}
