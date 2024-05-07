73854046f469e9f2db5c4c293a9cb37e2601cab2
Sid <oxyprogrammer@gmail.com>
2024-05-07 13:14:09 +0530

    Completed the command controllers.

--- SM-Post/Post.Query/Post.Query.Infrastructure/Handlers/EventHandler.cs
+++ SM-Post/Post.Query/Post.Query.Infrastructure/Handlers/EventHandler.cs
@@ -75,7 +75,7 @@ namespace Post.Query.Infrastructure.Handlers
 
         public async Task On(CommentRemovedEvent @event)
         {
-            await _commentRepository.DeleteAsync(@event.Id);
+            await _commentRepository.DeleteAsync(@event.CommentId);
         }
 
         public async Task On(PostRemovedEvent @event)
