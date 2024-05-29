51072f618911775cc258b4878eff3e7bc05e1934
Sid <oxyprogrammer@gmail.com>
2024-05-29 12:58:34 +0530

    Corrected Dockerfiles and also a route in the look up controller.

--- src/Post.Query/Post.Query.Api/Controllers/PostLookupController.cs
+++ src/Post.Query/Post.Query.Api/Controllers/PostLookupController.cs
@@ -18,35 +18,40 @@ namespace Post.Query.Api.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult> GetAllPostsAsync()
+        public async Task<IActionResult> GetAllPostsAsync()
         {
             var posts = await _queryDispatcher.SendAsync(new FindAllPostQuery());
             return NormalResponse(posts);
         }
 
-        [HttpGet("byId/{postId:guid}")]
-        public async Task<ActionResult> GetPostByIdAsync(Guid postId)
+        [HttpGet("{postId:guid}")]
+        public async Task<IActionResult> GetPostByIdAsync(Guid postId)
         {
             var posts = await _queryDispatcher.SendAsync(new FindPostByIdQuery { Id = postId });
-            return NormalResponse(posts);
+            if (posts == null || !posts.Any())
+            {
+                return NoContent();
+            }
+            return Ok(posts.First());
+            
         }
 
         [HttpGet("byAuthor/{author}")]
-        public async Task<ActionResult> GetPostsByAuthorAsync(string author)
+        public async Task<IActionResult> GetPostsByAuthorAsync(string author)
         {
             var posts = await _queryDispatcher.SendAsync(new FindPostsByAuthorQuery { Author = author });
             return NormalResponse(posts);
         }
 
         [HttpGet("withComments")]
-        public async Task<ActionResult> GetPostsWithCommentsAsync()
+        public async Task<IActionResult> GetPostsWithCommentsAsync()
         {
             var posts = await _queryDispatcher.SendAsync(new FindPostsWithCommentsQuery());
             return NormalResponse(posts);
         }
 
         [HttpGet("withLikes/{numberOfLikes:int}")]
-        public async Task<ActionResult> GetPostsWithLikesAsync(int numberOfLikes)
+        public async Task<IActionResult> GetPostsWithLikesAsync(int numberOfLikes)
         {
             var posts = await _queryDispatcher.SendAsync(new FindPostsWithLikesQuery { NumberOfLikes = numberOfLikes });
             return NormalResponse(posts);
