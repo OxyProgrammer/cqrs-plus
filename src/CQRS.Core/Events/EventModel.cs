ced55f1b40e5af6204e6cb120e5664e4846b4f55
oxyprogrammer <oxyprogrammer@gmail.com>
2024-11-05 19:10:42 +0530

    Reverted mongo db driver version.

--- src/CQRS.Core/Events/EventModel.cs
+++ src/CQRS.Core/Events/EventModel.cs
@@ -1,11 +1,12 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace CQRS.Core.Events
 {
     public class EventModel
     {
         [BsonId]
-        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
         public DateTime TimeStamp { get; set; }
